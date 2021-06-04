006d1a9c99e171b79168404cdf93036113e9be82
razkey23 <44244958+razkey23@users.noreply.github.com>
2021-06-04 09:03:04 +0300

    Updated Unified Endpoints

--- backend/Authenticator/routes/index.js
+++ backend/Authenticator/routes/index.js
@@ -14,8 +14,6 @@ const  request = require('request');
 
 
 
-
-
 passport.use('bcryptsignin',new LocalStrategy(function(username, password, cb) {
 
     //GET USER BY USERNAME
@@ -143,5 +141,203 @@ router.post('/register', function(req, res, next) {
     });
 });
 
+
+/* UNIFY ALL ENDPOINTS OF BACKEND */
+
+//Questions Per KW
+router.get('/QuestionsPerKW',function (req, res, next) {
+    if (!req.body.keyword && !req.body.id) {
+        res.json({"error":"Not keyword given"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:4000/QuestionsPerKW",
+            method: 'GET',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 200) {
+                res.json(body);
+            } else {
+                console.log(response.statusCode);
+            }
+        });
+    }
+});
+
+/*QUESTIONS PER DAY
+ INPUT 1 day or From Date , To Date
+ Input :
+        {
+          day : "2020-06-01"
+        }
+
+       or
+
+        {
+          {datefrom :""},
+          {dateto:""}
+        }
+ */
+router.get('/QuestionsPerDay',function (req, res, next) {
+    if (!req.body.day && (!req.body.datefrom || !req.body.dateto)) {
+        res.json({"error":"Bad Parameters in body"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:4000/QuestionsPerDay",
+            method: 'GET',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 200) {
+                res.json(body);
+            } else {
+                console.log(response.statusCode);
+            }
+        });
+    }
+});
+
+
+//Get answers of a question
+//GET request at localhost:5000/answerOf + question in body ("question":id) as int
+router.get('/AnswersOfQuestion',function(req,res,next) {
+    if (!req.body.question) {
+        res.json({"error": "Not keyword given"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:5000/AnswersOfQuestion",
+            method: 'GET',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 200) {
+                res.json(body);
+            } else {
+                console.log(response.statusCode);
+            }
+        });
+
+    }
+});
+
+//Get Answers per User
+// endpoint localhost:5000/AnswerOfUser + user in body ("keyword":id) as int
+router.get('/AnswersOfUser',function(req,res,next) {
+    if (!req.body.user) {
+        res.json({"error": "Not keyword given"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:5000/AnswersOfUser",
+            method: 'GET',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 200) {
+                res.json(body);
+            } else {
+                console.log(response.statusCode);
+            }
+        });
+
+    }
+});
+
+//Post Question
+/*
+ Post localhost:5000/AddQuestion + body following the format
+ {
+    "text": "",
+    "title":"",
+    "user":{
+      "id": "",
+      ...
+     },
+     "date_asked":"",
+     "keyword":[
+      { "id": ""},
+      { "id":""} ...
+     ]
+ }
+
+ Text,title,user,date_asked are necessary ,keyword is optional
+*/
+router.post('/AddQuestion',function(req,res,next){
+    if (!req.body.user.id || !req.body.title || !req.body.text || !req.body.date_asked){
+        res.json({"error":"No user given"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:5000/AddQuestion",
+            method: 'POST',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 201) {
+                res.status(201).send({"status":"OK"});
+            } else {
+                res.send({"Status":"Error Occured inserting a Question"})
+            }
+        });
+    }
+});
+
+//Post Answer
+/* Post localhost:5000/AddAnswer + body the format
+  {
+    "user":{
+      "id":"",
+      ...
+    },
+    "question":{
+      "id":"",
+      ...
+    },
+    "text":"",
+    "date_answered":""
+  }
+
+  All Fields are mandatory
+ */
+router.post('/AddAnswer',function(req,res,next){
+    if (!req.body.user.id || !req.body.text || !req.body.question.id || !req.body.date_answered){
+        res.json({"error":"No user given"});
+    }
+    else {
+        const jsonObject=req.body;
+        const requestOptions = {
+            url: "http://localhost:5000/AddAnswer",
+            method: 'POST',
+            json : jsonObject
+        }
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 201) {
+                res.status(201).send({"status":"OK"});
+            } else {
+                res.send({"Status":"Error Occured inserting an Answer"})
+            }
+        });
+    }
+});
 
 module.exports = router;
